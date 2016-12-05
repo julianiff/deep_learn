c623dfe28590204a3de95942beabdd4a232fdfc1
julianiff <julian@seerow.ch>
2016-12-05 15:17:32 +0100

    Added run.sh, but still a bit buggy

--- shell/run.sh
+++ shell/run.sh
@@ -1,1 +1,13 @@
-"If we want to have a run.sh"
+#If we want to have a runscript, at the moment a bit buggy
+#!/bin/bash
+
+source shell/config.yml
+
+echo "Lets run it (run.sh is a bit buggy sometimes, määh): "
+echo " "
+echo -n "Input filename >"
+read filename
+
+ssh $RATTLESSH .zshrc
+cd final
+python $filename
